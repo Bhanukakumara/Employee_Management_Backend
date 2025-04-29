89ecadc60b4f8f099bb827f5c1a52f6ed28a1fcd
Bhanukakumara <bhanukarasika@gmail.com>
2025-04-29 15:19:15 +0530

    employee controller full updated

--- src/main/java/edu/employee/controller/EmployeeController.java
+++ src/main/java/edu/employee/controller/EmployeeController.java
@@ -16,22 +16,32 @@ public class EmployeeController {
 
     final EmployeeService employeeService;
 
-    @PostMapping("/createEmployee")
+    @PostMapping("/create-employee")
     Employee createEmployee(@RequestBody EmployeeDto employeeDto){
         return employeeService.createEmployee(employeeDto);
     }
 
-    @GetMapping("/employees")
+    @GetMapping("/all-employees")
     List<EmployeeDto> getAll(){
         return employeeService.getAll();
     }
 
-    @PutMapping("/employees/{id}")
+    @GetMapping("/search-by-email/{email}")
+    EmployeeDto searchEmployeeByEmail(String email){
+        return employeeService.searchEmployeeByEmail(email);
+    }
+
+    @GetMapping("/search-by-id/{id}")
+    EmployeeDto searchEmployeeById(Long id){
+        return employeeService.searchEmployeeById(id);
+    }
+
+    @PutMapping("/update-employee-by-id/{id}")
     EmployeeDto updateEmployeeById(@PathVariable Long id, @RequestBody EmployeeDto employeeDto){
         return employeeService.updateEmployeeById(id, employeeDto);
     }
 
-    @DeleteMapping("/employees/{id}")
+    @DeleteMapping("/delete-employee-by-id/{id}")
     void deleteEmployeeById(@PathVariable Long id){
         employeeService.deleteEmployeeById(id);
     }
