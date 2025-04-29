5cc9b54072ab260f11fe900239970f69492fcffb
Bhanukakumara <bhanukarasika@gmail.com>
2025-04-29 10:19:50 +0530

    entity update

--- src/main/java/edu/employee/entity/Employee.java
+++ src/main/java/edu/employee/entity/Employee.java
@@ -1,14 +1,35 @@
 package edu.employee.entity;
 
 import edu.employee.utill.Department;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "employee")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
+
+    @Enumerated(EnumType.STRING)
     private Department department;
+
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
 }
