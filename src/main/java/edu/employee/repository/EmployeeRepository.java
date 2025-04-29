c12f714a9b03ced4c96799fb616641f5516b7bc7
Bhanukakumara <bhanukarasika@gmail.com>
2025-04-29 10:36:56 +0530

    employee repository

--- src/main/java/edu/employee/repository/EmployeeRepository.java
+++ src/main/java/edu/employee/repository/EmployeeRepository.java
@@ -1,4 +1,7 @@
 package edu.employee.repository;
 
-public interface EmployeeRepository {
+import edu.employee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
