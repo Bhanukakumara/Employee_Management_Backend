88bb3a83e963c9487692b13fa600a7c0dd80a8f3
Bhanukakumara <bhanukarasika@gmail.com>
2025-04-29 10:25:41 +0530

    entity decoration update

--- src/main/java/edu/employee/entity/Employee.java
+++ src/main/java/edu/employee/entity/Employee.java
@@ -2,11 +2,17 @@ package edu.employee.entity;
 
 import edu.employee.utill.Department;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "employee")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
 
     @Id
